f7fcddbba713cf513407161545b15b1c4a34834e
Kiyonna <kiyonna.kapoor@utexas.edu>
2023-10-08 21:37:43 -0500

    First push

---
+++ test_extensions/popup.js
@@ -0,0 +1,12 @@
+// popup.js
+document.addEventListener("DOMContentLoaded", function () {
+  const saveButton = document.getElementById("saveButton");
+  saveButton.addEventListener("click", function () {
+    // Your save button logic here
+    alert("Data saved!");
+  });
+});
+
+
+
+
